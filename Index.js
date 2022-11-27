cf461521c9cb34585c8bc3e7ae87e81d22eaac5f
Minhaz-Khan <kminhaz111@gmail.com>
2022-11-27 15:52:14 +0600

    get userType create done

--- Index.js
+++ Index.js
@@ -10,14 +10,31 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, (e, decoded) => {
+        if (e) {
+            return res.status(403).send({ message: 'forbidden' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.drtwsrz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         const categoreCollections = client.db('carClub').collection('categories');
-        const carsPostCollections = client.db('carClub').collection('carsPost')
-        const userCollections = client.db('carClub').collection('users')
+        const carsPostCollections = client.db('carClub').collection('carsPost');
+        const userCollections = client.db('carClub').collection('users');
+        const bookingCollections = client.db('carClub').collection('bookings');
 
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
@@ -36,7 +53,7 @@ async function run() {
             const result = await categoreCollections.find(query).toArray();
             res.send(result);
         })
-        app.get('/carspost/:id', async (req, res) => {
+        app.get('/carspost/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { categoriesId: id }
             const result = await carsPostCollections.find(query).toArray();
@@ -45,8 +62,39 @@ async function run() {
 
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const email = user.email;
+            const filter = {
+                email: email
+            }
+            const oldUser = await userCollections.findOne(filter);
+            if (oldUser) {
+                return res.send('user already exsist')
+            }
             const result = await userCollections.insertOne(user);
             res.send(result)
+
+        })
+        app.get('/users/type/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const user = await userCollections.findOne(filter);
+            res.send({ userType: user?.userType })
+        })
+
+        app.post('/bookings', verifyJWT, async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollections.insertOne(booking);
+            res.send(result);
+        })
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { buyerEmail: email };
+                const result = await bookingCollections.find(query).toArray();
+                return res.send(result);
+            }
+            res.status(403).send({ message: 'forbidden access' })
 
         })
 
