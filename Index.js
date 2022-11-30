da32d0498078b68e6827f5ef148b303308fc2607
Minhaz-Khan <kminhaz111@gmail.com>
2022-11-30 23:23:12 +0600

    done

--- Index.js
+++ Index.js
@@ -5,6 +5,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 app.use(cors());
 app.use(express.json());
@@ -242,6 +243,22 @@ async function run() {
             const query = { email: email };
             const result = await wishlistCollections.find(query).toArray();
             res.send(result);
+        })
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntent.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            })
         })
 
 
